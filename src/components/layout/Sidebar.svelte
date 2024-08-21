853f34700bb43e9ade4184900db74a9a9fa4ea96
Malin Schoening <malin.schoening@gmail.com>
2024-08-21 16:58:30 +0200

    initial commit

---
+++ src/components/layout/Sidebar.svelte
@@ -0,0 +1,38 @@
+<script>
+    import sidebar from "$lib/sidebar.js";
+
+</script>
+
+<aside class="position-fixed p-3 h-100">
+    <nav>
+        <ul class="p-2">
+            {#each sidebar as menuItem}
+                <li class="py-3">
+                    <a class="py-1 d-flex align-items-center" href={menuItem.url}>
+                    <svelte:component this={menuItem.icon} class="me-2" size="30" />
+                    {menuItem.name}</a>
+                </li>
+            {/each}
+        </ul>
+    </nav>
+</aside>
+
+<style>
+    aside {
+        background: var(--sidebarBackground);
+        color: var(--sidebarText);
+        width: 70px;
+    }
+
+
+    li {
+        font-size: 1.3rem;
+        font-weight: 200;
+    }
+
+    @media(min-width: 1200px){
+        aside {
+            width: 250px;
+        }
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+    import Footer from "../components/layout/Footer.svelte";
+    import Sidebar from "../components/layout/Sidebar.svelte";
+
+
+</script>
+
+<Sidebar />
+
+<main>
+    <slot />
+    <Footer />
+</main>
+
+<style>
+    main {
+        margin-left: 70px;
+    }
+
+    @media(min-width: 1200px){
+        main {
+            margin-left: 250px;
+        }
+    }
+</style>
