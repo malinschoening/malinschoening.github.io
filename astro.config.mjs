4a91bf0aed3f8f6baf8a92212c395e092782b1f7
Malin Schoening <malin.schoening@gmail.com>
2024-10-26 17:41:04 +0200

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte';
 
 export default defineConfig({
   site: 'https://malinschoening.github.io',
-  base: "/"
+  base: "/",
+  integrations: [svelte()],
 })
+
